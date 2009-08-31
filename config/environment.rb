f38fe6bac681b7e1900daf89d911c22174a3b3e9
Tyler Rick <github.com@tylerrick.com>
2009-08-31 16:36:46 -0700

    Moved ActionController::Base.session configuration to config/initializers/session_store.rb, where the standard rails generator would have created it.

--- config/environment.rb
+++ config/environment.rb
@@ -16,11 +16,6 @@ Rails::Initializer.run do |config|
   config.gem 'faker', :lib => false
   
   config.time_zone = 'UTC'
-
-  config.action_controller.session = {
-   :session_key => '_basejumper_session',
-   :secret      => '97ecd44e13fe9fc0bbcd6a6498ba77d60cd6174d242c62229f4efe84d89d5265ed1511f66ec87d499541f1566925d5f7d66a97979b267c54ca7784c1c1641f23'
-  }
 end
 
 require 'authlogic_system'
